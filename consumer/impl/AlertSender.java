a5f8c1c475f097f39a2aa43048eb411ea2d3a37d
dominic.stalder <dominic.stalder89@gmail.com>
2022-05-09 16:46:24 +0200

    r Webnavigator: Rename and rearrange web-navigating classes

--- consumer/impl/AlertSender.java
+++ consumer/impl/AlertSender.java
@@ -8,7 +8,7 @@ import com.aquabasilea.coursebooker.consumer.CourseBookingEndResultConsumer;
 import com.aquabasilea.coursebooker.states.CourseBookingState;
 import com.aquabasilea.i18n.TextResources;
 import com.aquabasilea.util.YamlUtil;
-import com.aquabasilea.web.selectcourse.result.CourseBookingEndResult;
+import com.aquabasilea.web.bookcourse.impl.select.result.CourseBookingEndResult;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
