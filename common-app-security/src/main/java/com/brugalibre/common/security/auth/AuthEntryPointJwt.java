7ca755a59fbf86a3cc8a97f7c0718d3dd8f2392b
dominic.stalder <dominic.stalder89@gmail.com>
2023-10-22 14:29:33 +0200

    f enable optional redirect if unauthorized access occurs

--- common-app-security/src/main/java/com/brugalibre/common/security/auth/AuthEntryPointJwt.java
+++ common-app-security/src/main/java/com/brugalibre/common/security/auth/AuthEntryPointJwt.java
@@ -4,9 +4,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
@@ -14,9 +16,16 @@ import java.io.IOException;
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
    private static final Logger LOG = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
+   @Value("${application.security.unauthorizedRedirect:}")
+   private String unauthorizedRedirect;
+
    @Override
    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-      LOG.error("Unauthorized request {}, error: {}", request.getRequestURL(), authException.getMessage());
-      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+      LOG.error("Unauthorized request {}, error: {}, unauthorizedRedirect: {}", request.getRequestURL(), authException.getMessage(), unauthorizedRedirect);
+      if (StringUtils.hasText(unauthorizedRedirect)) {
+         response.sendRedirect(unauthorizedRedirect);
+      } else {
+         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+      }
    }
 }
