f77153d154d103c54f0537d7853cfcf4543f78c5
dominic.stalder <dominic.stalder89@gmail.com>
2024-02-06 16:56:45 +0100

    r mobilephone api change url

--- common-app-domain/src/main/java/com/brugalibre/common/rest/api/mobilephone/MobilePhoneController.java
+++ common-app-domain/src/main/java/com/brugalibre/common/rest/api/mobilephone/MobilePhoneController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/mobilephone")
+@RequestMapping("/api/brugalibre/common/v1/mobilephone")
 public class MobilePhoneController {
 
    private final MobilePhoneChangeService mobilePhoneChangeService;
