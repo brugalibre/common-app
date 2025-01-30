ef1ed7f608eb79ac25b38d730249bd23afd62cfe
dominic <dominic.stalder89@gmail.com>
2025-01-30 19:54:15 +0100

    add rest-api in order to send messages

---
+++ common-app-notification/src/main/java/com/brugalibre/notification/app/AlertSendAppConfig.java
@@ -0,0 +1,11 @@
+package com.brugalibre.notification.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.brugalibre.notification"})
+public class AlertSendAppConfig {
+// no-op
+}
+
---
+++ common-app-notification/src/main/java/com/brugalibre/notification/app/AlertSendRestApplication.java
@@ -0,0 +1,12 @@
+package com.brugalibre.notification.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class AlertSendRestApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(AlertSendRestApplication.class, args);
+   }
+}
